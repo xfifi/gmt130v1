0a5ee61c3fe86f59ddee3b3d9abd91c9e4999962
Philippe RODIER <6016806+xfifi@users.noreply.github.com>
2023-10-23 12:43:40 +0200

    Update display.py

--- components/gmt130v1/display.py
+++ components/gmt130v1/display.py
@@ -1,2 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+
+from esphome import pins
+from esphome.components import display
+from esphome.const import (
+    CONF_HEIGHT,
+    CONF_ID,
+    CONF_LAMBDA,
+    CONF_WIDTH,
+    CONF_CS_PIN,
+    CONF_DC_PIN,
+    CONF_RESET_PIN,
+    CONF_NUMBER,
+)
+from esphome.const import __version__ as ESPHOME_VERSION
+from . import gmt130v1_ns
